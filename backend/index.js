f649b7498154c2cc129589cf6328b0d934209003
leanhit <theanh.it@gmail.com>
2025-06-23 11:51:32 +0000

    add code

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
